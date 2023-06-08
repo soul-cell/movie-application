525a6239795eea12c2da7ec03d184ec19551dd58
Thiru-pugazhi <thirupugazhi@anorasolutions.com>
2023-06-08 13:34:20 +0530

    movie crud

--- movie_application/logics/movie_logic.py
+++ movie_application/logics/movie_logic.py
@@ -1,6 +1,7 @@
 from movie_application.database import db_details
 from fastapi import APIRouter
 import pydantic
+from typing import Dict
 
 from bson.objectid import ObjectId
 
@@ -8,19 +9,17 @@ pydantic.json.ENCODERS_BY_TYPE[ObjectId]= str
 
 new_app = APIRouter()
 
-
 @new_app.get('/')
 def get_all_movie():
-    data = database.mycollection.find()
+    data=db_details.movies_collection.find()
     return list(data)
 
-
-@new_app.post("/")
-async def get_one_movie(values: Dict):
-    data = database.mycollection.find(values)
-    return data
+@new_app.post('/search')
+def post_movie(values:Dict):
+    data = db_details.movies_collection.find(values)
+    return list(data)
 
 @new_app.delete("/")
 async def delete_movie(value: Dict):
-    database.mycollection.find_one_and_delete(value)
+    db_details.movies_collection.find_one_and_delete(value)
     return {"data": []}
